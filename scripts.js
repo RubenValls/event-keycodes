eced87e9766e00f3385f32865e7770773607701f
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-23 08:38:09 +0200

    added logic to detect and print keycode, project working correctly

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 const container = document.querySelector('.container')
 
+const createElement = (element, key, span) => {
+    const newElement = document.createElement(`${element}`);
+    newElement.classList.add(key);
+    newElement.innerHTML = `<span>${span}</span>`;
+    return newElement;
+}
+
 onkeydown = (event) => {
-    console.log(event)
+    container.innerHTML = '';
+
+    const keyElement = createElement('div', 'key', 'event.key');
+    keyElement.innerHTML += event.key;
+    container.appendChild(keyElement);
+
+    const keyCodeElement = createElement('div', 'key', 'event.keyCode');
+    keyCodeElement.innerHTML += event.keyCode;
+    container.appendChild(keyCodeElement);
+
+    const code = createElement('div', 'key', 'event.code');
+    code.innerHTML += event.code;
+    container.appendChild(code);
 };
